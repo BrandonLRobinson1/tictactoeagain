51a1780305ed92bfac1a2a501d764b2c1952c29a
BrandonLRobinson1 <BrandonLRobinson1@gmail.com>
2017-03-13 11:01:54 -0700

    set up is complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('connected');
+
+let inquirer    = require('inquirer');
+let chalk       = require('chalk');
+let clear       = require('clear');
+let clui        = require('clui');
+let figlet      = require('figlet');
+let Preferences = require('preferences');
+let CLI         = require('clui');
+let Spinner     = CLI.Spinner;
+let GitHubAPI   = require('github');
+let _           = require('lodash');
+let git         = require('simple-git')();
+let touch       = require('touch');
+let fs          = require('fs');
+let files       = require('./lib/files');
+
+clear();
+console.log(
+  chalk.red(
+    figlet.textSync('TicTacToe', { horizontalLayout: 'full'} )
+    )
+  )
